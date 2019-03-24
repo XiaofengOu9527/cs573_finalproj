5dd263105a24aa92fd7b091efac05cb4bbc77aaf
ouxf17 <ouxiaofeng17@gmail.com>
2019-03-24 16:35:42 -0400

    first commit

--- preprocess.py
+++ preprocess.py
@@ -17,19 +17,57 @@ data = pd.read_csv(input_file)
 
 all_cols = data.columns.values.tolist()
 
-drop_cols = ['ID', 'Name', 'Photo', 'Flag', 'Club Logo', 'Loaned From', 
-             ]
+target_col = ['Value']
 
+
+# =============================================================================
+# drop some columns
+# =============================================================================
+drop_cols = ['Unnamed: 0', 'ID', 'Name', 'Photo', 'Flag', 'Overall', 
+			 'Club Logo', 'Wage', 'Real Face',
+			 'Jersey Number', 'Joined', 'Loaned From', 'Release CLause'
+             ]
 
 data = data.drop(drop_cols, axis=1)
 data = data.dropna(axis=0, how='any')
 
-discretize_cols = ['Age', 'Overall', 'Potential', 'Value', 'Wage', 'Special', 
+
+
+
+
+
+encoding_cols = ['Nationality', 'Preferred Foot', 'Work Rate', 
+				 'Body Type', 'Position',
+				 ]
+
+
+standardize_cols = ['Height', 'Weight']
+
+
+
+discretize_cols = ['Age', 'Potential', 'Club', 'Special' 
                    ]
 
-skill_cols = ['LS', 'ST', 'RS', 'LW', 'LF', 'CF', 'RF', 'RW', 'LAM', 'LCM', 'RAM',
-              'LM', 'LCM',
-                   ]
 
-encoding_cols = [
-                   ]
+
+
+# standardize then bin
+position_cols = ['LS', 'ST', 'RS', 'LW', 'LF', 'CF', 'RF', 'RW', 'LAM', 'CAM', 'RAM',
+              'LM', 'LCM', 'CM', 'RCM', 'RM', 'LWB', 'LDM', 'CDM', 'RDM', 'RWB',
+			  'LB', 'LCB', 'CB', 'RCB', 'RB']
+
+
+skill_cols = ['Crossing', 'Finishing', 'HeadingAccuracy',
+			  'ShortPassing', 'Volleys', 'Dribbling', 'Curve', 
+			  'FKAccuracy', 'LongPassing', 'BallControl', 'Acceleration', 'SprintSpeed', 
+			  'Agility', 'Reactions', 'Balance', 'ShotPower', 'Jumping', 'Stamina', 
+			  'Strength', 'LongShots,Aggression', 'Interceptions', 'Positioning', 
+			  'Vision', 'Penalties', 'Composure', 'Marking', 'StandingTackle', 
+			  'SlidingTackle', 'GKDiving', 'GKHandling', 'GKKicking', 
+			  'GKPositioning', 'GKReflexes']
+
+
+
+
+
+data.to_csv("data_processed.csv", index=False)
